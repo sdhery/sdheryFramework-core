c3d0c0dedfec5381cdb2ebbddd2d5a1db7360d4d
sdhery@gmail.com <herys@163.com>
2013-05-23 17:51:50 +0800

    提交类

---
+++ src/main/java/com/sdhery/core/domain/SysObjectKey.java
@@ -0,0 +1,29 @@
+package com.sdhery.core.domain;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sdhery
+ * Date: 13-5-23
+ * Time: 下午5:14
+ * To change this template use File | Settings | File Templates.
+ */
+public class SysObjectKey {
+    String sysObjectKey;
+    Integer sysObjectKeyValue;
+
+    public String getSysObjectKey() {
+        return sysObjectKey;
+    }
+
+    public void setSysObjectKey(String sysObjectKey) {
+        this.sysObjectKey = sysObjectKey;
+    }
+
+    public Integer getSysObjectKeyValue() {
+        return sysObjectKeyValue;
+    }
+
+    public void setSysObjectKeyValue(Integer sysObjectKeyValue) {
+        this.sysObjectKeyValue = sysObjectKeyValue;
+    }
+}
