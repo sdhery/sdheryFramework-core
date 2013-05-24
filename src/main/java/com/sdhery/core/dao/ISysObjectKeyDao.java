82a1b245919cbbb3e6482ba9c94e379739c1f7d0
sdhery@gmail.com <herys@163.com>
2013-05-24 18:02:06 +0800

    提交代码

---
+++ src/main/java/com/sdhery/core/dao/ISysObjectKeyDao.java
@@ -0,0 +1,13 @@
+package com.sdhery.core.dao;
+
+import com.sdhery.core.domain.SysObjectKey;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sdhery
+ * Date: 13-5-24
+ * Time: 下午4:25
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ISysObjectKeyDao extends EntityDao<SysObjectKey,String>  {
+}
---
+++ src/main/java/com/sdhery/core/dao/impl/SysObjectKeyDao.java
@@ -0,0 +1,14 @@
+package com.sdhery.core.dao.impl;
+
+import com.sdhery.core.dao.ISysObjectKeyDao;
+import com.sdhery.core.domain.SysObjectKey;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sdhery
+ * Date: 13-5-24
+ * Time: 下午4:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class SysObjectKeyDao extends BaseMybatisDao<SysObjectKey,String> implements ISysObjectKeyDao {
+}
