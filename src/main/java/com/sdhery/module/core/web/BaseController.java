de75f6022617e3d9bc178c758a4ae6905d22b8c8
sdhery@gmail.com <herys@163.com>
2013-06-07 10:22:08 +0800

    提交代码

---
+++ src/main/java/com/sdhery/module/core/web/BaseController.java
@@ -0,0 +1,33 @@
+package com.sdhery.module.core.web;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.springframework.ui.ModelMap;
+
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sdhery
+ * Date: 13-6-7
+ * Time: 上午10:12
+ * To change this template use File | Settings | File Templates.
+ */
+public class BaseController {
+    public void copyProperties(Object target, Object source) {
+        try {
+            BeanUtils.copyProperties(target, source);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setSuccess(ModelMap model) {
+        model.put("success", "true");
+    }
+
+    public void setFailure(ModelMap model) {
+        model.put("success", "false");
+    }
+}
